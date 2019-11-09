7114b9de8da4a23c1ad727a6984f92693251cd50
tom-balcaen-ahodos <tom.balcaen@ahodos.be>
2019-11-09 09:33:42 +0100

    created chart container and detail

--- d3-chart/src/app/app.module.ts
+++ d3-chart/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartContainerComponent } from './chart-container/chart-container.component';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartContainerComponent,
+    ChartComponent
   ],
   imports: [
     BrowserModule,
